35fd4bdf2f586d8391cb4d6b491389f76e067d66
shehan2006711 <154323632+shehan2006711@users.noreply.github.com>
2025-02-14 22:45:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'HANSAMAL-MD=E0o0kAoD#mZQfCqdZ9Ybp4e2452Hg1uwt_xrdmySmVVp6M9V4GBs' : process.env.SESSION_ID,
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'shehan2006711' : process.env.GITHUB_USER_NAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN,        
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94711262551' : process.env.OWNER_NUMBER
 };
